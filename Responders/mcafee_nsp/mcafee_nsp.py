be446825b29debffd95d5f25c67382974afc9f63
Sh4dd0w <49755704+shaddowdaniel@users.noreply.github.com>
2021-10-18 18:06:20 -0300

    Update mcafee_nsp.py

--- Responders/mcafee_nsp/mcafee_nsp.py
+++ Responders/mcafee_nsp/mcafee_nsp.py
@@ -1,39 +1,86 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 # Author: Daniel Müller
-
 from cortexutils.responder import Responder
 import requests
 import json
 import base64
+import os
 
 
 class McafeeNSP(Responder):
     def __init__(self):
         Responder.__init__(self)
-        self.host = self.get_param("config.url", "localhost")
+        self.host = self.get_param("config.host", "localhost")
         self.user = self.get_param("config.user", "user", None)
         self.pwd = self.get_param("config.pass", "pwd", None)
-
+        self.service = self.get_param("config.service", None)
+        self.data = self.get_param("data.data")
     def run(self):
         Responder.run(self)
-        toencode = self.user+self.pwd
-        self.basic =  base64.b64encode(toencode.encode('ascii'))
-        url = "https://"+self.host+"/sdkapi/session"
+        token = self.user+":"+self.pwd
+        token = token.encode('ascii')
+        self.basic =  base64.b64encode(token)
+        url = "https://"+self.host+"/sdkapi/"
         payload={}
-
         headers = {
         'Content-Type': 'application/json',
         'Accept': 'application/vnd.nsm.v1.0+json',
         'NSM-SDK-API': self.basic
         }
+        action = url+"session"
+        login = requests.get(action, headers=headers, data=payload, verify=False).json()
+        
+        
+        login_str = json.dumps(login)
+        token = json.loads(login_str)['session']+":"+json.loads(login_str)['userId']
+        token = token.encode('ascii')
+        self.basic =  base64.b64encode(token)
+        headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/vnd.nsm.v1.0+json',
+        'NSM-SDK-API': self.basic
+        }
+        if self.service == "lock":
+            ruleObject = "173"
+            action = url+"ruleobject/"+ruleObject
+            listRuleObject = requests.get(action, headers=headers, data=payload, verify=False).json()
+            resp = json.dumps(listRuleObject)
 
-        login = requests.request("GET", url, headers=headers, data=payload)
+            payload = {
+            'RuleObjDef': {
+            'visibleToChild': True, 
+            'description': listRuleObject['RuleObjDef']['description'], 
+            'ruleobjId': ruleObject, 
+            'name': listRuleObject['RuleObjDef']['name'], 
+            'ruleobjType': 'NETWORK_IPV_4',
+            'Network_IPV_4': listRuleObject['RuleObjDef']['Network_IPV_4']
+            }
+            }
+            payload['RuleObjDef']['Network_IPV_4']['networkIPV4List'].append(self.data)
+            payload = json.dumps(payload)
+            insertRuleObject = requests.put(action, headers=headers, data=payload, verify=False).json()
+            self.report(insertRuleObject) 
 
-        if login.status_code == 200:
-            self.report(login.text)
-        else:
-            self.error(login.status_code)
+        elif self.service == "unlock":
+            ruleObject = "173"
+            action = url+"ruleobject/"+ruleObject
+            listRuleObject = requests.get(action, headers=headers, data=payload, verify=False).json()
+            payload = {
+            'RuleObjDef': {
+            'visibleToChild': True,
+            'description': listRuleObject['RuleObjDef']['description'], 
+            'ruleobjId': ruleObject, 
+            'name': listRuleObject['RuleObjDef']['name'], 
+            'ruleobjType': 'NETWORK_IPV_4',
+            'Network_IPV_4': listRuleObject['RuleObjDef']['Network_IPV_4']
+            }
+            }
+            payload['RuleObjDef']['Network_IPV_4']['networkIPV4List'].remove(self.data)
+            payload = json.dumps(payload)
+
+            removeRuleObject = requests.put(action, headers=headers, data=payload, verify=False).json()
+            self.report(removeRuleObject)
 
 if __name__ == '__main__':
     McafeeNSP().run()
