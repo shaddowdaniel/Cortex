332aafce2bf6a71bf0f436fff1536f79af480a03
Sh4dd0w <49755704+shaddowdaniel@users.noreply.github.com>
2022-03-07 17:29:31 -0300

    Update okta.py

--- Responders/okta/okta.py
+++ Responders/okta/okta.py
@@ -16,24 +16,42 @@ class okta(Responder):
         self.user = self.get_param("config.user", "user", None)
         self.pwd = self.get_param("config.pass", "pwd", None)
         self.api_key = self.get_param("config.api_key", "api_key", None)
+        self.service = self.get_param("config.service", None)
+        data = self.get_param("data.data")
 
     def run(self):
         Responder.run(self)
-        url = "https://"+self.host+"/api/v1/users?q="+username+"&limit=1"
+        data = self.get_param("data.data")
+        data = str(data)
+        url = "https://docktech.okta.com/api/v1/users?q="+data+"&limit=1"
         payload={}
-        
+
         headers = {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
         'Authorization': 'SSWS '+self.api_key
         }
-        
-        find_userid = requests.request("GET", url, headers=headers, data=payload)
-        
-        if find_userid.status_code == 200:
-            self.report(find_userid.text)
+
+        find_userid = requests.get(url, headers=headers, json=payload).json()
+        #find_userid = json.loads(find_userid)
+        #if find_userid["status_code"] == 200:
+        userid = str(find_userid[0]['id'])
+        status = str(find_userid[0]['status'])
+
+        if self.service == "lock" and status == "ACTIVE":
+             url = "https://docktech.okta.com/api/v1/users/"+userid+"/lifecycle/suspend"
+             lock = requests.post(url, headers=headers, json=payload)
+
+             self.report({'status_code': str(lock.status_code),
+                 'message': 'User '+data+' success '+self.service})
+
+        elif self.service == "unlock" and status == "SUSPENDED":
+             url = "https://docktech.okta.com/api/v1/users/"+userid+"/lifecycle/unsuspend"
+             unlock = requests.post(url, headers=headers, json=payload)
+             self.report({'status_code': str(unlock.status_code),
+                 'message': 'User '+data+' success '+self.service})
         else:
-            self.error(find_userid.status_code)
+            self.error(self.service+" Error User:"+data+' Status '+status)
 
 if __name__ == '__main__':
     okta().run()
